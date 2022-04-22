a0bf9306e0025b6ea830e6c30d1ce237aeb8ba8d
fran124 <fherrera@lifia.info.unlp.edu.ar>
2022-04-22 20:31:54 +0000

    fixeado

--- promocion/tp2/index.py
+++ promocion/tp2/index.py
@@ -2,13 +2,51 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 import urllib
+import socket  # For gethostbyaddr()
 
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = 8010
 
 
 class MyServer(BaseHTTPRequestHandler):
+
+    metodos_validos = ('GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH')
+
+   # metodos_permitidos = ('GET')
+
+    def handle_one_request(self):
+
+        try:
+            self.raw_requestline = self.rfile.readline(65537)
+            if len(self.raw_requestline) > 65536:
+                self.requestline = ''
+                self.request_version = ''
+                self.command = ''
+                self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
+                return
+            if not self.raw_requestline:
+                self.close_connection = True
+                return
+            if not self.parse_request():
+                # An error code has been sent, just exit
+                return
+
+            if self.command in self.metodos_validos:
+                if self.command == 'GET':
+                    self.do_GET()
+                else:
+                    self.send_error(code=405, message="Metodo no permitido")
+            else:
+                self.send_error(code=400, message="Mala peticion")
+
+            # actually send the response if not already done.
+            self.wfile.flush()
+        except socket.timeout as e:
+            # a read or a write timed out.  Discard this connection
+            self.log_error("Request timed out: %r", e)
+            self.close_connection = True
+            return
 
     def do_GET(self):
         self.performReq(urllib.parse.unquote(self.path))
@@ -28,7 +66,8 @@ class MyServer(BaseHTTPRequestHandler):
             print('file '+fname+" Ok")
         except IOError:
             print('no file '+fname)
-            self.send_error(code=404, message="A donde queres ir titan galactico?")
+            self.send_error(
+                code=404, message="A donde queres ir titan galactico?")
 
 
 if __name__ == "__main__":
@@ -42,4 +81,3 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
-
