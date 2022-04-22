65819b9520f1072263907faca2614d6a3e1df382
fran124 <fherrera@lifia.info.unlp.edu.ar>
2022-04-22 18:35:40 +0000

    implementado 200 y 404

--- promocion/tp2/index.py
+++ promocion/tp2/index.py
@@ -2,21 +2,37 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
+from urllib import urlunquote
+
+
 hostName = "localhost"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Tp1 - Redes 2021 - Grupo Z</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Todo Ok.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
 
-if __name__ == "__main__":        
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.performReq(unlunquote(self.path).decode('utf-8'))
+
+    def performReq(self, req):
+        curDir = os.getcwd()
+        fname = curDir + '/' + self.path[1:]
+        try:
+            self.send_response(200, "Ok!")
+            ext = os.path.splitext(self.path)[1]
+            self.send_header('Content', 'text/xml; charset=UTF-8')
+            self.end_headers()
+            f = open(fname, 'rb')
+            for l in f:
+                self.wfile.write(l)
+            f.close()
+            print('file '+fname+" Ok")
+        except IOError:
+            print('no file '+fname)
+            self.send_error(code=404, message="A donde queres ir titan galactico?")
+
+
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
