84295094668f8607bcf9b3bc7ba453cfbf1bb9dd
fran124 <fherrera@lifia.info.unlp.edu.ar>
2022-04-24 21:28:43 +0000

    ejercicio completamente funcional (con algunos extras) TODO: -mejorar comentarios -instrucciones de uso -casos de pruebas

---
+++ promocion/tp2/TP2-EJ12.md
@@ -0,0 +1,57 @@
+12. (Ejercicio de promoción) Programar en el lenguaje que sea de su elección un servidor HTTP/1.0
+reducido que sólo implementará lo enumerado a continuación:
+NOTA: para quienes hagan la promoción, este será un ejercicio “entregable”. En la entrega deberán
+incluir el código fuente debidamente comentado y un README, o README.md que explique cómo
+generar el programa, cómo ejecutarlo y cómo testearlo.
+a. Deberá recibir requerimientos HTTP 1.0 y 1.1 para procesarlos y determinar sí la solicitud es válida.
+Si detecta que el requerimiento no es válido responderá con un error 400.
+b. Sólo debe implementar el método GET. Sí el método es otro responderá con un error 405.
+c. La respuesta será una página HTML generada automáticamente que incluye en su cuerpo el string
+del recurso HTTP solicitado.
+d. La respuesta HTTP debe ser válida, respetando el protocolo, e incluír las cabeceras apropiadas para
+indicar el tipo de contenido, el tamaño, la fecha y el tipo de servidor.
+e. Todas las cabeceras envíadas por el cliente deben ser válidas y en principio ignoradas.
+f. Debe ser programado utilizando la API de socket, es decir las llamadas, socket, listen, accept,
+etc.
+g. El servidor debe ser testeado con un navegador/user-agent http tradicional. Por ejemplo, sí ejecutamos el servidor localmente en puerto 80 las siguientes serían algunas interacciones posibles.
+
+$ telnet localhost 80
+GET /index.html HTTP/1.0
+HTTP/1.0 200 OK
+Date: Wed, 16 Mar 2022 15:26 GMT
+Server: Silly/0.1
+Content-Length: 33
+Content-Type: text/html
+Connection: Closed
+<HTML><H1>/index.html</H1></HTML>
+Connection closed by foreign host.
+Ejemplo 1
+$ telnet localhost 80
+GET dir/subdir/test.html HTTP/1.1
+Host: 127.0.0.1:8000
+Connection: keep-alive
+User-Agent: Mozilla/5.0 (X11; Linux x86_64)
+HTTP/1.0 200 OK
+Date: Wed, 16 Mar 2022 15:39 GMT
+Server: Silly/0.1
+Content-Length: 42
+Content-Type: text/html
+Connection: Closed
+<HTML><H1>dir/subdir/test.html</H1></HTML>
+Connection closed by foreign host.
+
+$ telnet localhost 80
+FFF /
+HTTP/1.0 400 Bad Request
+Connection closed by foreign host.
+Ejemplo 3
+$ telnet localhost 80
+....GET
+HTTP/1.0 400 Bad Request
+Connection closed by foreign host.
+Ejemplo 4
+$ telnet localhost 80
+HEAD /index.html HTTP/1.0
+HTTP/1.0 405 Method Not Allowed
+Connection closed by foreign host.
+Ejemplo 5
--- promocion/tp2/server.py
+++ promocion/tp2/server.py
@@ -1,88 +1,169 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-import urllib
+from time import time
 import socket
+import http.client
 from http import HTTPStatus
+import sys
+from email.utils import formatdate
 
-hostName = "localhost"
-serverPort = 8010
 
+class HTTPServer:
+    """  Soporta versiones HTTP/1.0 y HTTP/1.1 !!
+    si protocol_version = "HTTP/1.0", la conexion se cierra, salvo que el cliente
+    envie en cada consulta, el encabezado Connection: keep-alive
 
-class MyServer(BaseHTTPRequestHandler):
+    si protocol_version = "HTTP/1.1", la conexion NO se cierra, salvo que el cliente
+    envie el encabezado Connection: close
 
-    metodos_validos = ('GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH')
+    por defecto mantiene la conexion de inactividad con el socket de 100 segundos
+    """
+
+    # lo que el server asume si el cliente NO lo proporciona
+    default_request_version = "HTTP/1.0"
+    protocol_version = "HTTP/1.0"  # la version con la que trabaja el servidor siempre
+    timeout = 100  # cant de segundos de inactividad, None para infinito
+
+    def __init__(self, socket, host, port):
+
+        self.host = host
+        self.port = port
+        self.header = ''
+        self.socket = socket
+        print("Esperando que se conecte un cliente")
+        self.connection, self.address = self.socket.accept()  # blocking wait for client
+        print("Cliente conectado")
+        self.setup()
+        self.rfile = self.connection.makefile('rb', 0)
+        self.request = self.connection
+        try:
+            self.handle()
+        finally:
+            self.connection.close()
+            self.finish()
+
+    def default_close_connection(self):
+        if self.protocol_version == 'HTTP/1.1':
+            return False
+        return True
+
+    def setup(self):
+        if self.timeout is not None:
+            self.connection.settimeout(self.timeout)
+
+    def handle(self):
+        self.handle_one_request()
+        while (not self.close_connection):
+            self.handle_one_request()
+
+    def finish(self):
+        self.rfile.close()
 
     def handle_one_request(self):
-
+        self.close_connection = self.default_close_connection()
         try:
             self.raw_requestline = self.rfile.readline(65537)
             if len(self.raw_requestline) > 65536:
                 self.requestline = ''
                 self.request_version = ''
                 self.command = ''
-                self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
+                connection.send(HTTPStatus.REQUEST_URI_TOO_LONG)
+                # self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
                 return
+
             if not self.raw_requestline:
                 self.close_connection = True
                 return
-            if not self.parse_request():
-                # An error code has been sent, just exit
+
+            if (self.raw_requestline.strip() == b''):
+                # si el cliente ingresa blancos, no
+                # cerramos la conexion si estamos en 1.1
                 return
 
-            if self.command in self.metodos_validos:
-                if self.command == 'GET':
-                    self.do_GET()
-                else:
-                    self.send_error(code=405, message="Metodo no permitido")
-            else:
-                self.send_error(code=400, message="Mala peticion")
+            if not self.parse_request() or \
+                    not self.handle_command():
+                # An error code has been sent, just exit
+                self.close_connection = True
+                return
 
-            # actually send the response if not already done.
-            self.wfile.flush()
         except socket.timeout as e:
-            # a read or a write timed out.  Discard this connection
-            self.log_error("Request timed out: %r", e)
+            # a read or a write timed out. Discard this connection
+            sys.stderr.write("%s - - %s\n" % (self.address[0], e))
             self.close_connection = True
-        return
+            return
 
-    
+    def parse_request(self):
+        self.command = None  # set in case of error on the first line
+        self.request_version = version = self.default_request_version
+        requestline = str(self.raw_requestline, 'iso-8859-1')
+        self.requestline = requestline.rstrip('\r\n')
+        words = requestline.split()
+        if len(words) == 0:
+            return False
+        if len(words) >= 3:  # Suficiente para determinar la version de protocolo
+            version = words[-1]
+            try:
+                if version not in ('HTTP/1.0', 'HTTP/1.1'):
+                    raise ValueError
+            except (ValueError, IndexError):
+                error = "\n400, Bad request version: (%r)\n" % version
+                self.connection.send(error.encode('utf-8'))
+                return False
+            if version == 'HTTP/1.1' and self.protocol_version == "HTTP/1.1":  # evaluar si eliminar
+                self.close_connection = False
+            self.request_version = version
 
+        if not 2 <= len(words) <= 3:
+            rta = '%s 400 BAD REQUEST\n' % self.protocol_version
+            self.connection.send(
+                rta.encode('utf-8')
+            )
+            return False
+        # Guardamos en variables de instancia el comando y la ruta
+        self.command, self.path = words[:2]
+        # Leemos los headers
+        self.headers = http.client.parse_headers(self.rfile)
+        # Examinamos los encabezados para detectar la directiva Connection.
+        conntype = self.headers.get('Connection', "")
+        if conntype.lower() == 'close':
+            self.close_connection = True
+        elif (conntype.lower() == 'keep-alive'):
+            self.close_connection = False
+        return True
 
+    def handle_command(self):
+        metodos_validos = ('GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH')
+        # formato de fecha segun RFC 2822
+        date_time = formatdate(time(), usegmt=True)
 
-
-    def do_GET(self):
-        self.performReq(urllib.parse.unquote(self.path))
-
-    def performReq(self, req):
-        curDir = os.getcwd()
-        fname = curDir + '/' + self.path[1:]
-        try:
-            self.send_response(200, "Ok!")
-            ext = os.path.splitext(self.path)[1]
-            self.send_header('Content', 'text/xml; charset=UTF-8')
-            self.end_headers()
-            f = open(fname, 'rb')
-            for l in f:
-                self.wfile.write(l)
-            f.close()
-            print('file '+fname+" Ok")
-        except IOError:
-            print('no file '+fname)
-            self.send_error(
-                code=404, message="A donde queres ir titan galactico?")
-
-
-
+        success = True
+        if self.command in metodos_validos:
+            if (self.command == 'GET'):
+                rta = '%s 200 OK\n' % self.protocol_version
+                rta += 'Date: %s\n' % date_time
+                rta += 'Server: Redes-2021/grupo-z\n'
+                rta += 'Content-Length: 00\n'
+                rta += 'Content-Type: text/html\n'
+                rta += 'Connection: %s\n\n' % (
+                    'close' if self.close_connection else 'keep-alive')
+                rta += '<HTML><H1>%s</H1></HTML>\n\n' % self.path
+            else:
+                success = False
+                rta = '%s 405 METHOD NOT ALLOWED\n' % self.protocol_version
+        else:
+            success = False
+            rta = '%s 400 BAD REQUEST\n' % self.protocol_version
+        self.connection.send(rta.encode('utf-8'))
+        return success
 
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    host = 'localhost'
+    port = 8010
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind((host, port))
+    srv.listen(1)
+    print("Server iniciado http://%s:%s" % (host, port))
+    while True:
+        HTTPServer(srv, host, port)
+    print("Server detenido.")
